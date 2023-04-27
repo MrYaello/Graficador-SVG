51140ec16fbaa9d51d0e052fad2b7c38cdfdf1bc
MrYaello <lozanoy6@gmail.com>
2023-04-27 03:34:32 -0600

    Implementación pila

--- src/main/java/mx/unam/ciencias/edd/proyecto2/graphers/GrapherStructure.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto2/graphers/GrapherStructure.java
@@ -1,6 +1,5 @@
 package mx.unam.ciencias.edd.proyecto2.graphers;
 
-import java.util.Iterator;
 import mx.unam.ciencias.edd.*;
 import mx.unam.ciencias.edd.proyecto2.*;
 
@@ -15,12 +14,12 @@ public class GrapherStructure extends GrapherSVG {
     private int WIDTH;
     private int HEIGHT;
     private int ELEMENT_WIDTH;
-    private final int MIN_ELEMENT_WIDTH;
-    private int ELEMENT_HEIGHT;
     private int RADIUS;
-    private int MARGIN;
-    private int PADDING;
-    private int FONT_SIZE;
+    private final int MIN_ELEMENT_WIDTH;
+    private final int ELEMENT_HEIGHT;
+    private final int MARGIN;
+    private final int PADDING;
+    private final int FONT_SIZE;
 
     public GrapherStructure() {
         MIN_ELEMENT_WIDTH = 40;
@@ -66,28 +65,26 @@ public class GrapherStructure extends GrapherSVG {
     public String graphLineal(boolean doubleArrows) {
         String conectorDeclaration = doubleArrows ? declareArrows() : declareArrow();
         String s = "";
-        String value;
         String[] elements = doubleArrows ? build.toString().substring(1, build.toString().length() - 1).split(", ")
                                          : build2.toString().split(",");
-        if (elements[0] != null && elements[0] != "") {
-            value = elements[0];
-            ELEMENT_WIDTH = calcElementWidth(value);
+        if (elements[0] != null && !elements[0].equals("")) {
+            ELEMENT_WIDTH = calcElementWidth(elements[0]);
             WIDTH += 2 * MARGIN + ELEMENT_WIDTH;
             HEIGHT = 2 * MARGIN + ELEMENT_HEIGHT;
-            s += drawRectText(MARGIN, MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", value);
+            s += drawRectText(MARGIN, MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", elements[0]);
         } else {
             s += drawEmpty();
             WIDTH = 2 * MARGIN + 40;
             HEIGHT = 2 * MARGIN + 40;
+            return s;
         }
-        if (elements[1] != null && elements[1] != "") s += conectorDeclaration;
+        s += conectorDeclaration;
         for (int i = 1; i < elements.length; i++) {
-            value = String.valueOf(elements[i]);
-            ELEMENT_WIDTH = calcElementWidth(value);
+            ELEMENT_WIDTH = calcElementWidth(elements[i]);
             WIDTH += PADDING;
             s += doubleArrows ? drawArrows(WIDTH - MARGIN, 0) : drawArrow(WIDTH - MARGIN, 0);
             WIDTH += PADDING + 24;
-            s += drawRectText(WIDTH - MARGIN, MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", value);
+            s += drawRectText(WIDTH - MARGIN, MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", elements[i]);
             WIDTH += ELEMENT_WIDTH;
         }
         return s;
@@ -99,6 +96,23 @@ public class GrapherStructure extends GrapherSVG {
 
     public String graphPila() {
         String s = "";
+        String[] elements = build2.toString().split("\n");
+        if (elements[0] != null && !elements[0].equals("")) {
+            System.out.println(max(elements));
+            ELEMENT_WIDTH = calcElementWidth(max(elements));
+            WIDTH += 2 * MARGIN + ELEMENT_WIDTH;
+            HEIGHT = 2 * MARGIN + ELEMENT_HEIGHT;
+            s += drawRectText(MARGIN, MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", elements[0]);
+        } else {
+            s += drawEmpty();
+            WIDTH = 2 * MARGIN + 40;
+            HEIGHT = 2 * MARGIN + 40;
+        }
+        for (int i = 1; i < elements.length; i++) {
+            HEIGHT += PADDING;
+            s += drawRectText(MARGIN, HEIGHT - MARGIN, ELEMENT_WIDTH, ELEMENT_HEIGHT, "Black", "White", FONT_SIZE, "Black", elements[i]);
+            HEIGHT += ELEMENT_HEIGHT;
+        }
         return s;
     }
 
@@ -118,5 +132,17 @@ public class GrapherStructure extends GrapherSVG {
 
     private int calcElementWidth(String value) {
         return (value.length() * 14) + (2 * PADDING) > 40 ? (value.length() * 14) + (2 * PADDING) : MIN_ELEMENT_WIDTH;
+    }
+
+    private String max(String[] arr) {
+        int max = Integer.MIN_VALUE;
+        int pos = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < Integer.parseInt(arr[i])) {
+                max = Integer.parseInt(arr[i]);
+                pos = i;
+            }
+        }
+        return arr[pos];
     }
 }
